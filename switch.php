4ec59b5cb8be59585084474a15da444389ff25eb
DENI ANDRIANTO <70729593+Den1Andr1anto@users.noreply.github.com>
2020-10-06 12:56:48 +0700

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,21 @@
+<?php
+$gitf = 4;
+
+switch ($gitf){
+    case 1:
+        echo "Dapat course gratis";
+    break;
+
+    case 2:
+        echo " dapat kaos gratis";
+    break;
+
+    case 3:
+        echo "dapat flashdisk gratis";
+    break;
+
+    default:
+        echo "isikan yang benar ya";
+break;
+}
+?>
